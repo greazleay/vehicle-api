3aa992687e34bd751319bbd6a112937af3a6c16d
Lekan Adetunmbi <lothbroch@gmail.com>
2022-10-13 21:56:41 +0100

    Added Vehicle and Manufacturer Models

--- migrations/migrate.go
+++ migrations/migrate.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"github.com/greazleay/vehicle-api/initializers"
+	"github.com/greazleay/vehicle-api/models"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
+
+func main() {
+	initializers.DB.AutoMigrate(&models.Vehicle{})
+}
--- models/vehicleModel.go
+++ models/vehicleModel.go
@@ -2,10 +2,26 @@ package models
 
 import "gorm.io/gorm"
 
+type Engine struct {
+	Cylinder   int
+	HorsePower float32
+	Torque     int
+}
+
 type Vehicle struct {
 	gorm.Model
+
 	Make         string
-	Mod          string
+	VModel       string
+	Category     string
 	Year         int
 	NumberOfSeat int
+	Price        int
+	Engine       Engine `gorm:"embedded"`
+}
+
+type Maanufacturer struct {
+	gorm.Model
+
+	Name string
 }
