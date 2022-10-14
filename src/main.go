d1c3402d223277eeb928625598cdc21426dfc71b
Lekan Adetunmbi <lothbroch@gmail.com>
2022-10-14 22:22:57 +0100

    Enabled Route Grouping in main package

--- src/main.go
+++ src/main.go
@@ -17,13 +17,17 @@ func main() {
 
 	router.GET("/", controllers.Index)
 
-	router.POST("/makers", controllers.CreateMaker)
-	router.GET("/makers", controllers.GetAllMakers)
-	router.GET("/makers/:id", controllers.GetMakerByID)
-	router.PATCH("/makers/:id", controllers.UpdateMaker)
-	router.DELETE("/makers/:id", controllers.DeleteMaker)
+	makerRouter := router.Group("/v1/makers")
 
-	router.POST("/vehicles", controllers.CreateVehicle)
+	makerRouter.POST("/", controllers.CreateMaker)
+	makerRouter.GET("/", controllers.GetAllMakers)
+	makerRouter.GET("/:id", controllers.GetMakerByID)
+	makerRouter.PATCH("/:id", controllers.UpdateMaker)
+	makerRouter.DELETE("/:id", controllers.DeleteMaker)
+
+	vehicleRouter := router.Group("/v1/vehicles")
+
+	vehicleRouter.POST("/", controllers.CreateVehicle)
 
 	router.Run()
 }
